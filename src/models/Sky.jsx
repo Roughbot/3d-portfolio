7320b0f022fce0e36e12de035976a3413b7df8f0
Sathyanarayanan <sathyanarayanan1106@gmail.com>
2024-01-13 14:48:31 +0530

    added 3d model

---
+++ src/models/Sky.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Sky = () => {
+  return <mesh></mesh>;
+};
+
+export default Sky;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,15 +6,47 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import Loader from "../components/Loader";
+import Island from "../models/island";
+import Sky from "../models/sky";
 
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+    return [screenScale, screenPosition, rotation];
+  };
+
+  const [islandScale, islandPosition, islandRotation] =
+    adjustIslandForScreenSize();
+
   return (
     <section className="w-full h-screen relative">
       <Canvas
         className="w-full h-screen bg-transparent"
         camera={{ near: 0.1, far: 1000 }}
       >
-        <Suspense fallback={<Loader />}></Suspense>
+        <Suspense fallback={<Loader />}>
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          <hemisphereLight
+            skyColor="#b1e1ff"
+            groundColor="#000000"
+            intensity={1}
+          />
+          <Sky />
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
+          />
+        </Suspense>
       </Canvas>
     </section>
   );
