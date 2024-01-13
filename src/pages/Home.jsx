3c4fa9b3feb09253815d3071c3dad5ccdc9d4eaf
Sathyanarayanan <sathyanarayanan1106@gmail.com>
2024-01-13 14:15:45 +0530

    assets files

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+{
+  /* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+  POPUP
+</div> */
+}
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Loader from "../components/Loader";
+
+const Home = () => {
+  return (
+    <section className="w-full h-screen relative">
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}></Suspense>
+      </Canvas>
+    </section>
+  );
+};
+
+export default Home;
