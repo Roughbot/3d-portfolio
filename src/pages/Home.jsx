a139801b48a5bbdf2d08a098669811e3878dec2e
Sathyanarayanan <sathyanarayanan1106@gmail.com>
2024-01-13 19:15:02 +0530

    bird and plane animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,8 @@ import Plane from "../models/Plane";
 
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false);
+  const [currentStage, setCurrentStage] = useState(1);
+
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
     let screenPosition = [0, -6.5, -43];
@@ -60,13 +62,14 @@ const Home = () => {
             intensity={1}
           />
           <Bird />
-          <Sky />
+          <Sky isRotating={isRotating} />
           <Island
             position={islandPosition}
             scale={islandScale}
             rotation={islandRotation}
             isRotating={isRotating}
             setIsRotating={setIsRotating}
+            setCurrentStage={setCurrentStage}
           />
           <Plane
             isRotating={isRotating}
