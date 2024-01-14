65e07c74a17878f04ac32cde799d6f4aa0079baf
Sathyanarayanan <sathyanarayanan1106@gmail.com>
2024-01-14 18:37:23 +0530

    bug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { Canvas } from "@react-three/fiber";
 import { Suspense, useState, useEffect, useRef } from "react";
 import Loader from "../components/Loader";
 import Island from "../models/island";
-import Sky from "../models/sky";
+import Sky from "../models/Sky";
 import Bird from "../models/Bird";
 import Plane from "../models/Plane";
 import HomeInfo from "../components/HomeInfo";
