e8c145cf749f57630a4223f2b9dcf250b110deca
Sathyanarayanan <sathyanarayanan1106@gmail.com>
2024-01-16 21:15:24 +0530

    small update

--- src/constants/index.js
+++ src/constants/index.js
@@ -122,19 +122,6 @@ export const experiences = [
       "Participating in code reviews and providing constructive feedback to other developers.",
     ],
   },
-  {
-    title: "React Native Developer",
-    company_name: "Tesla",
-    icon: tesla,
-    iconBg: "#fbc3bc",
-    date: "Jan 2021 - Feb 2022",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
 ];
 
 export const socialLinks = [
